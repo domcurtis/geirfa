d43dbf443c1294224c02f51c4809b55bc6aae69d
domcurtis <domcurtis@users.noreply.github.com>
2026-03-22 17:07:28 +0000

    Deploy preview for PR 21 🛫

--- pr-preview/pr-21/github-pages-deploy-action-temp-deployment-folder/pr-preview/pr-21/github-pages-deploy-action-temp-deployment-folder/pr-preview/pr-21/github-pages-deploy-action-temp-deployment-folder/sw.js
+++ pr-preview/pr-21/github-pages-deploy-action-temp-deployment-folder/pr-preview/pr-21/github-pages-deploy-action-temp-deployment-folder/pr-preview/pr-21/github-pages-deploy-action-temp-deployment-folder/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'geirfa-v7';
+const CACHE = 'geirfa-v4';
 const PRECACHE = ['./index.html', './vocabulary.json', './favicon.svg', './apple-touch-icon.png', './manifest.json'];
 
 self.addEventListener('install', e => {
