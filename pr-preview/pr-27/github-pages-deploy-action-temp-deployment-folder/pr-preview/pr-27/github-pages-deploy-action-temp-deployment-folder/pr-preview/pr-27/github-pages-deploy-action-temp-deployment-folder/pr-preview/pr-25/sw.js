b7076578467553a568434b755c322331003acf3d
domcurtis <domcurtis@users.noreply.github.com>
2026-03-31 20:07:26 +0000

    Deploy preview for PR 27 🛫

---
+++ pr-preview/pr-27/github-pages-deploy-action-temp-deployment-folder/pr-preview/pr-27/github-pages-deploy-action-temp-deployment-folder/pr-preview/pr-27/github-pages-deploy-action-temp-deployment-folder/pr-preview/pr-25/sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'geirfa-v9';
+const PRECACHE = ['./index.html', './vocabulary.json', './favicon.svg', './apple-touch-icon.png', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(PRECACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
+
+self.addEventListener('message', e => {
+  if (e.data === 'getVersion') e.ports[0].postMessage(CACHE);
+});
